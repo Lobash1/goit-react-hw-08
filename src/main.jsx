e2d0de7f7a49e140052149bbf5b841c5c6993b0c
Nastya <lobash.n@gmail.com>
2025-04-08 21:58:52 +0300

    to work

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import "./index.css";
 import App from "./components/App/App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
